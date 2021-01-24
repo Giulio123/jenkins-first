d09a93a9c59ab5eba2f588999a77f81f52fd7ba7
Giulio123 <guaraldino@hotmail.it>
2021-01-24 16:12:41 +0100

    commit for check poll SCM trigger

--- src/main/java/com/sugfdo/jenkinsfirst/JenkinsFirst.java
+++ src/main/java/com/sugfdo/jenkinsfirst/JenkinsFirst.java
@@ -5,9 +5,15 @@ import org.springframework.stereotype.Service;
 /*JenkinsFirst logic*/
 @Service
 public class JenkinsFirst {
+	
 
 	@Cacheable("sum")
 	public int sum(int a, int b) {
 		return a + b;
+	}
+	
+	@Cacheable("diff")
+	public int diff(int a, int b) {
+		return a - b;
 	}
 }
